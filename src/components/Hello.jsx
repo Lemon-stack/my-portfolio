dcd00fa632e27b64c430f6a86c69972c7bc73cab
Lemon <lemonconfidence101@gmail.com>
2024-11-07 17:28:00 +0000

    changed introductory text

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -5,23 +5,24 @@ export default function Hello() {
         Hello, I&apos;m Lemon Confidence
       </h2>
       <p className="text-green-500 mb-3">
-        Frontend developer <span className="text-gray-50">(ReactJs)</span>
+        Frontend developer
+        {/* <span className="text-gray-50"></span> */}
       </p>
 
       <div className="pt-3">
         <p className="text-gray-200 font-thin">
-          I&apos;m a React developer with a strong foundation in modern
-          technologies like Tailwind CSS, Supabase, Firebase, Git and GitHub.
+          I&apos;m a Self-taught web developer with a strong foundation in
+          modern technologies like NextJs, ReactJs, Tailwind CSS, Supabase,
+          Firebase, Git and GitHub.
           <br />
+          I&apos;m also comofortable with UI libraries and State Management
+          tools like Redux Toolkit, ShadCN and Aceternity UI
           <br />
           While React is my primary focus, I also use Tailwind CSS for stying,
           Git &amp; GitHub for version control and Firebase for my backend
           <br />
-          I&apos;m currently learning new stacks like NextJs and also ExpressJs,
-          seeking to improver efficiently in my field of work.
-          <br />
-          I&apos;ve also volunteered for some large scale project which
-          you&apos;d soon see in the project&apos;s page of this webapp.
+          I&apos;m currently learning new stacks like React-Native and also
+          ExpressJs, seeking to improver efficiently in my field of work.
           <br />
           <br />
           BTW - I&apos;m on the lookout for a role as a Frontend Developer.
